dd710f8b7424dde8d63d03b54756b1fdd999067f
KirillBaranov <k.baranov@forpeople.company>
2022-09-25 01:23:29 +0300

    Fix eslint bug.

---
+++ src/components/core/FormBuilder/FormField/FormField.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="form-field"></div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
---
+++ src/components/core/FormBuilder/FormLevel/FormLevel.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="form-level"></div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
