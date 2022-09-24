3137ea990831507c3b41487f46f5934bf258bb84
KirillBaranov <k.baranov@forpeople.company>
2022-09-25 01:38:50 +0300

    Create props/emits.

--- src/components/core/FormBuilder/FormBuilder.vue
+++ src/components/core/FormBuilder/FormBuilder.vue
@@ -1,7 +1,41 @@
 <template>
-  <form class="form-builder"></form>
+  <form class="form-builder">
+    <FormLevel
+      v-for="level in schema"
+      :level="level"
+      :key="level.id"
+      @updateModelValue="updateModelValue"
+    />
+  </form>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { PropType } from "vue";
+import type { Schema } from "@/components/core/FormBuilder/Schema/schema";
+import type { Model } from "@/components/core/FormBuilder/Model/types";
+
+import FormLevel from "@/components/core/FormBuilder/FormLevel/FormLevel.vue";
+import _ from "lodash";
+
+const props = defineProps({
+  schema: {
+    type: Object as PropType<Schema>,
+    required: true,
+  },
+  model: {
+    type: Object as PropType<Model>,
+    required: true,
+  },
+});
+
+const emits = defineEmits(["update:modelValue"]);
+
+const updateModelValue = (modelPath: string, value: any) => {
+  const proxyModel = _.cloneDeep(props.model);
+  _.set(proxyModel, modelPath, value);
+
+  emits("update:modelValue", proxyModel);
+};
+</script>
 
 <style lang="scss" scoped></style>
