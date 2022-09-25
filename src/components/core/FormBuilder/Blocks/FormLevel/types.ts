e895362d81031b1c7217d22e819d4e1b17e5e19b
KirillBaranov <k.baranov@forpeople.company>
2022-09-25 20:48:02 +0300

    Update Level interface/create AllFieldVariants.

--- src/components/core/FormBuilder/Blocks/FormLevel/types.ts
+++ src/components/core/FormBuilder/Blocks/FormLevel/types.ts
@@ -1,6 +1,19 @@
 import type { Field } from "@/components/core/FormBuilder/Blocks/FormField/types";
+import type { BuildAllWidgets } from "@/components/core/FormBuilder/Widgets/widgets";
+
+type AllFieldVariants = Field<BuildAllWidgets>;
+
+type AllChildrenVariants =
+  | AllFieldVariants[]
+  | Array<AllFieldVariants[] | Level[]>
+  | Level[];
 
 export interface Level {
+  /**
+   * Service field
+   */
+  type: "level";
+
   /**
    * Level ID
    */
@@ -14,5 +27,5 @@ export interface Level {
   /**
    * Children nodes
    */
-  children?: Array<Field[] | Level[]>;
+  children: AllChildrenVariants;
 }
