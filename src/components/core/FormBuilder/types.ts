d3fa7b94b9fa695600d85ef03870518f1cca06cd
KirillBaranov <k.baranov@forpeople.company>
2022-10-02 23:44:20 +0300

    Feat(core-types): Add UpdateModelValue type.

--- src/components/core/FormBuilder/types.ts
+++ src/components/core/FormBuilder/types.ts
@@ -2,5 +2,9 @@ import type { Level } from "@/components/core/FormBuilder/Blocks/FormLevel/types
 
 export type Model = Record<string, any>;
 export type ExtraModel = Record<string, any>;
+export interface UpdateModelValuePayload {
+  modelPath: string;
+  value: any;
+}
 
 export type Schema = Level[];
