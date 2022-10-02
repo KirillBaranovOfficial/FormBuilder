b6dc023f6330daeb1f370ab332a18f474e0384cf
KirillBaranov <k.baranov@forpeople.company>
2022-10-02 22:51:13 +0300

    Refactor(core): update config widget imports.

--- src/components/core/FormBuilder/Config/config.ts
+++ src/components/core/FormBuilder/Config/config.ts
@@ -1,17 +1,19 @@
 import type { WidgetConfig } from "@/components/core/FormBuilder/Config/types";
 
+import TextInput from "@/components/ui/Widgets/TextInput/TextInput.vue";
+import CheckboxInput from "@/components/ui/Widgets/CheckboxInput/CheckboxInput.vue";
+
 export const widgetsConfig: Map<string, WidgetConfig> = new Map([
   [
     "CheckboxInput",
     {
-      template: () =>
-        import("@/components/ui/Widgets/CheckboxInput/CheckboxInput.vue"),
+      template: CheckboxInput,
     },
   ],
   [
     "TextInput",
     {
-      template: () => import("@/components/ui/Widgets/TextInput/TextInput.vue"),
+      template: TextInput,
     },
   ],
 ]);
