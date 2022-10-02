6353083636cf93f8e547db406f6a66cd91c75825
KirillBaranov <k.baranov@forpeople.company>
2022-10-02 22:51:43 +0300

    Refactor(FormBuilder): Rename model -> modelValue.

--- src/components/core/FormBuilder/FormBuilder.vue
+++ src/components/core/FormBuilder/FormBuilder.vue
@@ -4,6 +4,8 @@
       v-for="level in schema"
       :level="level"
       :key="level.id"
+      :model="modelValue"
+      :extraModel="extraModel"
       @updateModelValue="updateModelValue"
     />
   </form>
@@ -26,7 +28,7 @@ const props = defineProps({
     type: Object as PropType<Schema>,
     required: true,
   },
-  model: {
+  modelValue: {
     type: Object as PropType<Model>,
     required: true,
   },
@@ -39,7 +41,7 @@ const props = defineProps({
 const emit = defineEmits(["update:modelValue"]);
 
 const updateModelValue = (modelPath: string, value: any) => {
-  const proxyModel = _.cloneDeep(props.model);
+  const proxyModel = _.cloneDeep(props.modelValue);
   _.set(proxyModel, modelPath, value);
 
   emit("update:modelValue", proxyModel);
