5ed1b4a8391fc1d584380c49209a73d5dadfba0a
KirillBaranov <k.baranov@forpeople.company>
2022-10-02 23:42:46 +0300

    Feat(TextInput): Add v-model supporting.

--- src/components/ui/Widgets/TextInput/TextInput.vue
+++ src/components/ui/Widgets/TextInput/TextInput.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="widget widget__text-input">
-    <input type="text" />
+    <input type="text" v-model="proxyModelValue" />
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computed } from "vue";
+import _ from "lodash";
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const proxyModelValue = computed({
+  get: () => props.modelValue,
+  set: (next: string | number | null) => {
+    if (!_.isEqual(next, proxyModelValue)) {
+      emit("update:modelValue", next);
+    }
+  },
+});
+</script>
