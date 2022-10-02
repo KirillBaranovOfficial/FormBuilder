f9308f86a8552d153b208e8f7f8f7e66456f3ddf
KirillBaranov <k.baranov@forpeople.company>
2022-10-02 16:24:39 +0300

    Feat(types): Add ExtraModel type.

--- src/components/core/FormBuilder/types.ts
+++ src/components/core/FormBuilder/types.ts
@@ -1,5 +1,6 @@
 import type { Level } from "@/components/core/FormBuilder/Blocks/FormLevel/types";
 
 export type Model = Record<string, any>;
+export type ExtraModel = Record<string, any>;
 
 export type Schema = Level[];
