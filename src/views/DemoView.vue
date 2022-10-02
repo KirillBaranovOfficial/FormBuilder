33d6e0acf02cea87fb20d72dd26d0a4c5eb2fbf5
KirillBaranov <k.baranov@forpeople.company>
2022-10-02 22:52:23 +0300

    Feat(Demo): Add demo page.

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,5 +1,21 @@
-<template><main class="page page-demo"></main></template>
+<template>
+  <main class="page page-demo">
+    <h1>Demo FormBuilder</h1>
 
-<script lang="ts" setup></script>
+    <FormBuilder v-model="model" :schema="schema" />
+  </main>
+</template>
 
-<style scoped></style>
+<script lang="ts" setup>
+import type { Model, Schema } from "@/components/core/FormBuilder/types";
+import type { Ref } from "vue";
+import { ref } from "vue";
+import FormBuilder from "@/components/core/FormBuilder/FormBuilder.vue";
+import { demoSchema } from "@/schemas/demoSchema";
+
+const model: Ref<Model> = ref({
+  name: "test",
+});
+
+const schema: Ref<Schema> = ref([...demoSchema]);
+</script>
